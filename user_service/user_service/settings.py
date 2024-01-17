e49329905f5c1b9cc1ebef64eaa5e06934224352
saul-rocha <saul.rocha2001gmail.com>
2024-01-17 16:28:36 -0300

    pronto

--- user_service/user_service/settings.py
+++ user_service/user_service/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#e4h9r-y21#i6+_ak&j7+t5&erw13x4*a-c^bgp_pd@g=$f^xg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
